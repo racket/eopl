bec46d552ff169a0cf43df42002e3f56e518554d
Andrew Tappenden <andrew.tappenden@kingsu.ca>
2014-12-17 22:08:49 -0700

    Updating sllgen.rkt
    As per bug request #13831 (http://bugs.racket-lang.org/query/?debug=&database=default&cmd=view+audit-trail&cmd=view&pr=13831)
    
    Since Racket v. 5.3.4, support for SLLGEN in EOPL package.
    
    This simple fix, re-enables the feature which is clearly listed in the documentation.
    http://docs.racket-lang.org/eopl/index.html?q=sllgen#%28def._%28%28lib._eopl%2Feopl..rkt%29._sllgen~3amake-rep-loop%29%29

---
+++ tests/sllgen/sllgenTests.rkt
@@ -0,0 +1,8 @@
+sllgen:make-string-parser
+sllgen:make-stream-parser
+sllgen:make-stream-scanner
+sllgen:make-string-scanner
+sllgen:make-define-datatypes
+sllgen:show-define-datatypes
+sllgen:list-define-datatypes
+sllgen:make-rep-loop
