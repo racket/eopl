6cdba82a22f13f7e0eb3da15b3dac3c9f77a4839
Andrew Tappenden <andrew.tappenden@kingsu.ca>
2014-12-17 22:17:35 -0700

    Cleaning up SLLGEN tests (testing that sllgen:make-rep-loop exists).

--- tests/sllgen/sllgenTests.rkt
+++ tests/sllgen/sllgenTests.rkt
@@ -1,10 +1,3 @@
 #lang eopl
 
-sllgen:make-string-parser
-sllgen:make-stream-parser
-sllgen:make-stream-scanner
-sllgen:make-string-scanner
-sllgen:make-define-datatypes
-sllgen:show-define-datatypes
-sllgen:list-define-datatypes
 sllgen:make-rep-loop
